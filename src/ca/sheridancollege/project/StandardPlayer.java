2c4def0afaadbc322af01944a5a8b5c7b7eedfc0
Vigneshwar-Premachandran <premachv@sheridancollege.ca>
2021-03-15 17:25:06 -0400

    Players can register to play.

---
+++ src/ca/sheridancollege/project/StandardPlayer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author vigne
+ */
+public class StandardPlayer extends Player{
+
+    public StandardPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public String toString(){
+        return this.getName();
+    }
+    
+}
