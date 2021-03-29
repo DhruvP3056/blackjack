fc0167e9cb24daf36331124bda0a50cfd7bf1657
amanda <amanda-jose@live.com>
2021-03-29 19:04:46 -0400

    Revert "updated"
    This reverts commit 0c3dcd702a0773d1987df4b5dbc89299b666f6ee.

--- src/ca/sheridancollege/project/BalanceCalculator.java
+++ src/ca/sheridancollege/project/BalanceCalculator.java
@@ -5,11 +5,5 @@ package ca.sheridancollege.project;
  * @author Amanda
  */
 public class BalanceCalculator {
-    public static final double MULTIPLIER = 1.5;
-    public static double winnings;
-    
-    public static void calculateBalance(StandardPlayer player){
-       winnings += player.getCurrentBet() * MULTIPLIER;
-       player.setWallet(player.getWallet() + winnings);
-    }
+    public final int MULTIPLIER = 1.5;
 }
