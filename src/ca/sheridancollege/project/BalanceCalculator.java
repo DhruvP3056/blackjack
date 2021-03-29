64c48ea2b551d1c3afefa168b78d4a289699b987
amanda <amanda-jose@live.com>
2021-03-29 19:12:58 -0400

    balance calculator class created

--- src/ca/sheridancollege/project/BalanceCalculator.java
+++ src/ca/sheridancollege/project/BalanceCalculator.java
@@ -5,5 +5,11 @@ package ca.sheridancollege.project;
  * @author Amanda
  */
 public class BalanceCalculator {
-    public final int MULTIPLIER = 1.5;
+    public static final double MULTIPLIER = 1.5;
+    public static double winnings;
+    
+    public static void calculateBalance(StandardPlayer player){
+       winnings += player.getCurrentBet() * MULTIPLIER;
+       player.setWallet(player.getWallet() + winnings);
+    }   
 }
